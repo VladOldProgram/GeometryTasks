d2710934205c2c0914d5250eaaaae941f18c9a91
Vladislav <86352975+VladOldProgram@users.noreply.github.com>
2022-10-06 18:23:33 +0300

    Add files via upload

---
+++ ReadOnlyVectorTask.cs
@@ -0,0 +1,30 @@
+﻿namespace ReadOnlyVectorTask
+{
+    public class ReadOnlyVector
+    {
+        public readonly double X, Y;
+
+        public ReadOnlyVector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public ReadOnlyVector Add(ReadOnlyVector other)
+        {
+            double x = this.X + other.X;
+            double y = this.Y + other.Y;
+            return new ReadOnlyVector(x, y);
+        }
+
+        public ReadOnlyVector WithX(double x)
+        {
+            return new ReadOnlyVector(x, this.Y);
+        }
+
+        public ReadOnlyVector WithY(double y)
+        {
+            return new ReadOnlyVector(this.X, y);
+        }
+    }
+}
---
+++ VectorTask.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Vector
+{
+    public class Vector
+    {
+        public double X, Y;
+
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public Vector Add(Vector v)
+        {
+            return Geometry.Add(this, v);
+        }
+
+        public bool Belongs(Segment s)
+        {
+            return Geometry.IsVectorInSegment(this, s);
+        }
+    }
+
+    public class Segment
+    {
+        public Vector Begin, End;
+
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public bool Contains(Vector v)
+        {
+            return Geometry.IsVectorInSegment(v, this);
+        }
+    }
+
+    public class Geometry
+    {
+        public static double GetLength(Vector v)
+        {
+            return Math.Sqrt(v.X * v.X + v.Y * v.Y);
+        }
+
+        public static double GetLength(Segment s)
+        {
+            var v = new Vector 
+	        { 
+		        X = s.End.X - s.Begin.X, 
+		        Y = s.End.Y - s.Begin.Y 
+	        };
+
+            return GetLength(v);
+        }
+
+        public static Vector Add(Vector v1, Vector v2)
+        {
+            return new Vector 
+	        { 
+		        X = v1.X + v2.X, 
+		        Y = v1.Y + v2.Y 
+	        };
+        }
+
+        public static bool IsVectorInSegment(Vector v, Segment s)
+        {
+            double segmentFullLength = GetLength(s);
+            var v2 = new Vector 
+	        { 
+		        X = v.X - s.Begin.X, 
+		        Y = v.Y - s.Begin.Y 
+	        };
+            double segmentFirstPartLength = GetLength(v2);
+            var v3 = new Vector 
+	        { 
+		        X = s.End.X - v.X, 
+		        Y = s.End.Y - v.Y 
+	        };
+            double segmentSecondPartLength = GetLength(v3);
+
+            return segmentFirstPartLength + segmentSecondPartLength == segmentFullLength;
+        }
+    }
+}
